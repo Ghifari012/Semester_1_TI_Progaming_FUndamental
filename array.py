32f5dbe6729d8ca1602b7079338afcaaec11a317
Ghifari012 <119100498+Ghifari012@users.noreply.github.com>
2022-11-26 15:19:35 +0700

    menambah fitur baru tentang insert array

--- array.py
+++ array.py
@@ -1,3 +1,6 @@
-print("hello world")
+
 #mengenal array
 Buah=["mangga", "apel", "pisang", "pepaya", "rambutan"]
+Nama=["soim", "ari", "rudi", "irvan", "phalosa"]
+#menambah nilai baru pada array
+Nama.insert(2,"miko")
